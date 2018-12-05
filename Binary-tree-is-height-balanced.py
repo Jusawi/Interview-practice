1b482c4bd657b54ebebf3701002cbe4382906243
Bala <38873553+Jusawi@users.noreply.github.com>
2018-12-05 13:31:00 +0530

    Update Binary-tree-is-height-balanced.py

--- Binary-tree-is-height-balanced.py
+++ Binary-tree-is-height-balanced.py
@@ -19,3 +19,26 @@ def is_balanced(root):
     return is_balanced(root.right) and \
     is_balanced(root.left) and \
     abs(get_height(root.left) - get_height(root.right)) <= 1
+
+
+#Better Method
+def is_balanced_helper(root):
+    # a None tree is balanced
+    if root is None:
+        return 0
+    left_height = is_balanced_helper(root.left)
+    # if the left subtree is not balanced, then:
+    # this tree is also not balanced
+    if left_height == -1:
+        return -1
+    # if the right subtree is not balanced, then:
+    # this tree is also not balanced
+    right_height = is_balanced_helper(root.right)
+    if right_height == -1:
+        return -1
+    # if the diffrence in heights is greater than 1, then:
+    # this tree is not balanced
+    if abs(left_height - right_height) > 1:
+        return -1
+    # this tree is balanced, return its height
+    return max(left_height, right_height) + 1
