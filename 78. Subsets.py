bca2aadf1d33390b08da3ffcb6d8cb653fd5fb14
Bala <38873553+Jusawi@users.noreply.github.com>
2019-01-01 13:32:24 +0530

    Create 78. Subsets.py

---
+++ 78. Subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+       def __init__(self):
+        self.res = []
+       
+       def subsets(self, nums, path=[]):
+           self.res.append(path)
+           for i in range(len(nums)):
+               self.subsets(nums[i+1:], path+[nums[i]])
+           return self.res
