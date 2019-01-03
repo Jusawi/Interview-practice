c1eca4b6113dcc238554e3b8931573b42cadb4c3
Bala <38873553+Jusawi@users.noreply.github.com>
2019-01-03 19:55:00 +0530

    Create 14. Longest Common Prefix.py

---
+++ strings/14. Longest Common Prefix.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs: return ''
+        first = min(strs)
+        for i in range(len(first)):
+            for s in strs:
+                if s[i] != first[i]:
+                    return first[:i] if i > 0 else ''
+        return first
+#pythonic solution
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]; rtype: str
+        """
+        sz, ret = zip(*strs), ""
+        # looping corrected based on @StefanPochmann's comment below
+        for c in sz:
+            if len(set(c)) > 1: break
+            ret += c[0]
+        return ret
