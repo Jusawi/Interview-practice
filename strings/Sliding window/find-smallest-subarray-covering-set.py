80041dbcb2e890c3f1efcd68ba2428a07b8c5538
Bala <38873553+Jusawi@users.noreply.github.com>
2019-01-16 18:27:36 +0530

    Create find-smallest-subarray-covering-set.py

---
+++ strings/Sliding window/find-smallest-subarray-covering-set.py
@@ -0,0 +1,22 @@
+import collections
+astr = ["apple", "banana", "apple", "apple", "dog", "cat", "apple", "dog", "banana",
+"apple", "cat", "dog"]
+search = ["banana","cat"]
+
+count = collections.Counter(search)
+res = (-1, -1) #final answer for this question is (8, 10)
+start = 0
+missing = len(search)
+for end,p in enumerate(astr):
+    if p in count and count[p] == 1:
+        count[p]-=1
+        missing -=1
+    while missing == 0:
+        if res == (-1,-1) or (res[1] - res[0] > end - start):
+            res = (start, end)
+        temp = astr[start]
+        start+=1
+        if temp in count:
+            count[temp]+=1
+            missing+=1
+print(res)
