d573b76e1c6d558347f95775b1afa4fea6821b46
Bala <38873553+Jusawi@users.noreply.github.com>
2019-01-17 22:22:54 +0530

    Update [lc]Longest substring without repeating characters.py

--- [lc]Longest substring without repeating characters.py
+++ [lc]Longest substring without repeating characters.py
@@ -1,16 +1,38 @@
-class Solution:
+def lengthOfLongestSubstring(self, s):
+    dic, res, start, = {}, 0, 0
+    for i, ch in enumerate(s):
+        if ch in dic:
+            # update the res
+            res = max(res, i-start)
+            # here should be careful, like "abba"
+            start = max(start, dic[ch]+1)
+        dic[ch] = i
+    # return should consider the last 
+    # non-repeated substring
+    return max(res, len(s)-start)
+
+
+#SLIDING WINDOW APPROACH -- LESS EFICIENT
+
+class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        start, res, dic = 0, 0, {}
-        #h = 0
-        for i,ch in enumerate(s):
-            if ch in dic:
-                res = max(res, i - start)
-                start = max(start, dic[ch]+1)
-                dic[ch] = i
-            else:
-                dic[ch] = i
-        return max(res, len(s) - start)
+        max_len, start, end, counter = 0, 0, 0, 0
+        char_dict = collections.defaultdict(int)
+        while end < len(s):
+            if char_dict[s[end]] > 0:
+                counter += 1
+            char_dict[s[end]] += 1
+            end += 1
+            while counter > 0:
+                char_dict[s[start]] -= 1
+                if char_dict[s[start]] > 0:
+                    counter -= 1
+                start += 1
+
+            max_len = max(max_len, end-start)
+        
+        return max_len
